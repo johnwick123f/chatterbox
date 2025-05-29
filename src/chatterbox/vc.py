ca1ff7e846324c7a1e393eb7d55773e92b8e0576
johnwick123f <128541091+johnwick123f@users.noreply.github.com>
2025-05-29 16:12:59 -0400

    Update vc.py

--- src/chatterbox/vc.py
+++ src/chatterbox/vc.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import librosa
 import torch
-import perth
+#import perth
 from huggingface_hub import hf_hub_download
 
 from .models.s3tokenizer import S3_SR
@@ -25,7 +25,7 @@ class ChatterboxVC:
         self.sr = S3GEN_SR
         self.s3gen = s3gen
         self.device = device
-        self.watermarker = perth.PerthImplicitWatermarker()
+        #self.watermarker = perth.PerthImplicitWatermarker()
         if ref_dict is None:
             self.ref_dict = None
         else:
@@ -84,5 +84,5 @@ class ChatterboxVC:
                 ref_dict=self.ref_dict,
             )
             wav = wav.squeeze(0).detach().cpu().numpy()
-            watermarked_wav = self.watermarker.apply_watermark(wav, sample_rate=self.sr)
-        return torch.from_numpy(watermarked_wav).unsqueeze(0)
+            #watermarked_wav = self.watermarker.apply_watermark(wav, sample_rate=self.sr)
+        return torch.from_numpy(wav).unsqueeze(0)
